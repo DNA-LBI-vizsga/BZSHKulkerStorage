d414c53c5b6f2664be129762c2aaa8c98485a9e7
Lévai Balázs <zsakosgandalf77@gmail.com>
2025-02-27 22:22:45 +0100

    Time zone changed for sql timestamps

--- BACKEND/config.js
+++ BACKEND/config.js
@@ -14,7 +14,8 @@ const config = {
 const sequelize = new Sequelize(config.db.database, config.db.user, config.db.password, {
     host: config.db.host,
     dialect: config.db.dialect,
-    logging: false
+    logging: false,
+    timezone: '+01:00'
 });
 
 module.exports = {
