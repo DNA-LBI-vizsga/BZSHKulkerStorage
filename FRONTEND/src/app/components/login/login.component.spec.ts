85f8d390c4f8be2d45febcf4e5cffd407b588b47
Dobany Norbert <ndobany@gmail.com>
2025-04-01 21:47:35 +0200

    angular material deleted

--- FRONTEND/src/app/components/login/login.component.spec.ts
+++ FRONTEND/src/app/components/login/login.component.spec.ts
@@ -1,14 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 
 import { LoginComponent } from './login.component';
+import { UserService } from '../../services/user/user.service';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let mockUserService: jasmine.SpyObj<UserService>;
 
   beforeEach(async () => {
+    mockUserService = jasmine.createSpyObj('UserService', ['loginUser', 'passwordChange']);
+
     await TestBed.configureTestingModule({
-      declarations: [LoginComponent]
+      declarations: [LoginComponent],
+      imports: [FormsModule],
+      providers: [{ provide: UserService, useValue: mockUserService }]
     })
     .compileComponents();
 
